785d0aaf6cd6beb1fd3d0abf03464dee567f5528
y203378 <wakana.yoshitomo.lc.tut.jp>
2020-12-08 17:22:34 +0900

    add name

--- s4/B203378/Hello.java
+++ s4/B203378/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello..(B203378,若菜慶友)");
+        System.out.println("hello..Wakana(B203378)");
     }
 }
 
